8a005a8bd56c9741d5a8791fab64c6c0cd4633fb
Kagan Batuker <batuker.kagan@windowslive.com>
2025-04-19 19:31:57 +0300

    add basic NFT functionality (mint and tokenUri)

--- src/BasicNFT.sol
+++ src/BasicNFT.sol
@@ -7,16 +7,22 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract BasicNft is ERC721 {
 
     uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenIdToUri;
 
-    constructor() ERC721("Duckie", "DUCK") {
+    constructor() ERC721("Doggo", "DOG") {
         s_tokenCounter = 0;
     }
 
-    function mintNft() public {}
+    function mintNft(string memory tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+        //call _safeMint function from openzeppelin
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
 
     //Universal Resource Identifier for the NFT. Basically, where all the required metadata is stored.
     function tokenURI(uint256 tokenId) public view override returns (string memory){
-        
+        return s_tokenIdToUri[tokenId];
     }
 
 }
